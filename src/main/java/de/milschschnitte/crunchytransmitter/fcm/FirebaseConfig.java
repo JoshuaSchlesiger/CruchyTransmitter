d48d7ab0d085281a00c038d3234be3251d5254cb
DochSergeantTV <joshua.schlesiger@steep.de>
2024-06-10 14:39:24 +0200

    used ClassPathResource to get fcm file

--- src/main/java/de/milschschnitte/crunchytransmitter/fcm/FirebaseConfig.java
+++ src/main/java/de/milschschnitte/crunchytransmitter/fcm/FirebaseConfig.java
@@ -6,15 +6,19 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/crunchy-transmitter-firebase-adminsdk-fondb-7cc6dde6c0.json");
+        Resource resource = new ClassPathResource("crunchy-transmitter-firebase-adminsdk-fondb-7cc6dde6c0.json");
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
